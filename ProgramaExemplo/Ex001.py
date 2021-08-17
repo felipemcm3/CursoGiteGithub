87d72e22e2b7f91e118eae72c8be09dcb547ad0c
Felipe <felipemcm3@gmail.com>
2021-08-17 10:37:45 -0300

    Divisão e multiplicação
    Adicionado na linha 5 divisão e linha 6 multiplicação

--- ProgramaExemplo/Ex001.py
+++ ProgramaExemplo/Ex001.py
@@ -2,3 +2,5 @@ print('\033[31mBem-Vindo ao Primeiro programa em GitHub e Visual Studio Core\033
 num = int(input('Informe um número'))
 num2 = int(input('Informe outro numero'))
 print(f'O valor {num} mais {num2} é {num+num2}')
+print(f'O valor {num} dividido por {num2} é {num/num2}')
+print(f'O valor {num} multiplicado por {num2} é {num*num2}')
