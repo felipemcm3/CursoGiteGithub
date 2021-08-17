c2967fe5aad6b3a5c9959ff3f138a7ce03baa0b5
Felipe <felipemcm3@gmail.com>
2021-08-17 09:46:39 -0300

    Primeiro exercicio
    Testando o github

---
+++ ProgramaExemplo/Ex001.py
@@ -0,0 +1,4 @@
+print('\033[31mBem-Vindo ao Primeiro programa em GitHub e Visual Studio Core\033[m')
+num = int(input('Informe um número'))
+num2 = int(input('Informe outro numero'))
+print(f'O valor {num} mais {num2} é {num+num2}')
